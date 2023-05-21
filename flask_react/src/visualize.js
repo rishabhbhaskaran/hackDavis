428513a0c99e19f42f3be1c14fb1aec0ce7552c1
Lily Bhattacharjee <lbhattacharjee@berkeley.edu>
2023-05-20 23:51:04 -0700

    Finalize basic functionality of visualize page

--- flask_react/src/visualize.js
+++ flask_react/src/visualize.js
@@ -6,24 +6,30 @@ import './visualize.css';
 import { createRoot } from 'react-dom/client';
 import Map from './map.js';
 
-function LayerItem(props) {
-    return (<div className="layer_item">
-        {props.projectName}
-        <br />
-        City of Davis
-    </div>)
-}
-
 function Visualize(props) {
     const [mapData, setMapData] = useState(null);
     const [locationData, setLocationData] = useState(null);
     const [savedLayers, setSavedLayers] = useState(null);
     const [savedLayerData, setSavedLayerData] = useState(null);
 
+    function LayerItem(props) {
+        return (
+            <div className={props.clicked ? "layer_item_clicked" : "layer_item"}>
+                <div onClick={() => {
+                    changeLayerInfo(props.id, !props.clicked)
+                }}>
+                    {props.name}
+                    <br />
+                    City of Davis
+                </div>
+            </div>
+        )
+    }
+
     function SavedLayers() {
         return (<div id="saved_layers">
             <h2>Saved Layers</h2>
-            {savedLayerData && savedLayerData.length > 0 && savedLayerData.map(item => <LayerItem key={item.name} projectName={item.name} />)}
+            {savedLayerData && savedLayerData.length > 0 && savedLayerData.map(item => <LayerItem key={item.name} name={item.name} id={item.id} clicked={item.clicked} />)}
         </div>);
     }
 
@@ -39,7 +45,7 @@ function Visualize(props) {
                 const currLayerId = res[i].layerId;
                 const currName = res[i].layerName ? res[i].layerName : '';
                 savedLayers.push(currLayerId);
-                savedLayerData.push({ 'name': currName });
+                savedLayerData.push({ 'name': currName, 'id': currLayerId, 'clicked': false });
             }
             setSavedLayerData([...savedLayerData]);
             setSavedLayers([...savedLayers]);
@@ -52,23 +58,64 @@ function Visualize(props) {
         })
     }
 
-    function getLayerInfo(layerId) {
-        console.log('entered function');
+    function changeLayerInfo(layerId, clicked) {
         axios({
             method: 'GET',
             url: '/getLayer?layerId=' + layerId
         }).then((response) => {
-            const res = response.data;
-            console.log('here', res);
-            return res;
+            const res = response.data['pins'];
+            const currPins = [];
+            for (let i = 0; i < res.length; i++) {
+                let currPin = res[i];
+                currPins.push([currPin.lat, currPin.long, currPin.name]);
+            }
+
+            let newSavedLayerData = [];
+            for (let i = 0; i < savedLayerData.length; i++) {
+                newSavedLayerData.push(savedLayerData[i]);
+                if (savedLayerData[i].id == layerId) {
+                    newSavedLayerData[i].clicked = !newSavedLayerData[i].clicked;
+                }
+            }
+
+            setSavedLayerData([...newSavedLayerData]);
+            let newMapData;
+            if (clicked) {
+                newMapData = [...mapData, ...currPins];
+            } else {
+                newMapData = [];
+                for (let i = 0; i < mapData.length; i++) {
+                    let flag = false;
+                    for (let j = 0; j < currPins.length; j++) {
+                        if ((currPins[j][2] == mapData[i][2])) {
+                            flag = true;
+                        }
+                    }
+                    if (!flag) {
+                        newMapData.push(mapData[i]);
+                    }
+                }
+            }
+
+            setMapData(newMapData);
         }).catch((error) => {
             if (error.response) {
                 console.log(error.response)
                 console.log(error.response.status)
                 console.log(error.response.headers)
             }
-            return {};
         })
+    }
+
+    function removeLayerInfo(layerId) {
+        let newMapData = [];
+        for (let i = 0; i < mapData.length; i++) {
+            newSavedLayerData.push(savedLayerData[i]);
+            if (savedLayerData[i].id == layerId) {
+                newSavedLayerData[i].clicked = !newSavedLayerData[i].clicked;
+            }
+        }
+        setSavedLayerData([...newSavedLayerData]);
     }
 
     function getLocation() {
@@ -81,7 +128,6 @@ function Visualize(props) {
     function showPosition(position) {
         var lat = position.coords.latitude;
         var long = position.coords.longitude;
-        console.log('lat in visualize', lat, 'long', long);
         setLocationData({ 'lat': lat, 'long': long });
         setMapData([['Lat', 'Long', 'Name'], [lat, long, 'Current Location']]);
     }
